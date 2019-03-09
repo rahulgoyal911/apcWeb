382107274b0c94b6aeab5f2cc93713344cf2de40
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-03-09 18:10:43 +0530

    attach box to cursor

--- mouseEvents/Script.js
+++ mouseEvents/Script.js
@@ -1,10 +1,16 @@
 
 var input=document.getElementById('inp');
+var body=document.getElementById('bd');
 var par=document.getElementById('p1');
-input.addEventListener("mouseover",function(event)
+input.addEventListener("mousemove   ",function(event)
 {
   inp.setAttribute("style","background-color: black;color:white");
   console.log(1);
+});
+body.addEventListener("mousemove",function(event)
+{
+  body.setAttribute("style","color:white");
+  inp2.setAttribute("style",`top: ${event.pageY}; left: ${event.pageX}`);
 });
 input.addEventListener("mouseout",function(event){
   inp.setAttribute("style","background-color: tomato;color:black");
@@ -13,16 +19,20 @@ input.addEventListener("mouseout",function(event){
 var input2=document.getElementById('inp2');
 input2.addEventListener("mouseover",function(event)
 {
-  inp2.setAttribute("style","background-color: black;color:white");
-  console.log(event.x);
-  console.log(event.y);
-  par.innerHTML=event.x;
-  par.innerHTML+=" ";
-  par.innerHTML+=event.x;
-  par.innerHTML+=" ";
-  par.innerHTML+=event.pageX;
-  par.innerHTML+=" ";
-  par.innerHTML+=event.pageY;
-  par.innerHTML+=" ";
+//   inp2.setAttribute("style","background-color: black;color:white");
+//   console.log(event.x);
+//   console.log(event.y);
+//   par.innerHTML="event x = ";
+//   par.innerHTML+=event.x;
+//   par.innerHTML+="\n";
+//   par.innerHTML+="event Y = ";
+//   par.innerHTML+=event.y;
+//   par.innerHTML+="\n";
+//   par.innerHTML+="coordinate X = ";
+//   par.innerHTML+=event.pageX;
+//   par.innerHTML+="\n";
+//   par.innerHTML+="coordinate Y = ";
+//   par.innerHTML+=event.pageY;
+  
 
 });
