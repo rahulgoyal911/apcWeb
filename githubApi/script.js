cf9b846f60fe26d32ab24bf282abd8ada618822b
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-03-30 08:13:53 +0530

    api

--- githubApi/script.js
+++ githubApi/script.js
@@ -1,5 +1,6 @@
 var btn=document.getElementById('btn');
 var input = document.getElementById("inp");
+var data = document.getElementById("data");
 btn.addEventListener("click",function(){
   var value = input.value;
   if(!value)
@@ -12,10 +13,21 @@ btn.addEventListener("click",function(){
 function getUser(val){
   console.log(val);
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.github.com/users/rahulgoyal911");
+  xhr.open("GET", "https://api.github.com/users/"+val);
   xhr.onload = function(){
     console.log(xhr.responseText);
-  };
+    var d = xhr.responseText;  
+    d = JSON.parse(xhr.responseText);
+    var firstPost = d['login'];
+    data.innerHTML = "username: " + firstPost;
+    data.innerHTML += "<br>";
+    firstPost = d['name'];
+    data.innerHTML += "name: " +  firstPost;
+    data.innerHTML += "<br>";
+    firstPost = d['bio'];
+    data.innerHTML += "bio: " + firstPost;
+    data.innerHTML += "<br>";
+};
   xhr.send();
 
 }
